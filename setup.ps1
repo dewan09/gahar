32d5f79cc0c2b4194d04397ca7d857bf241c8a27
dewan09 <140136125+dewan09@users.noreply.github.com>
2023-07-23 14:50:37 +0700

    setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,5 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+Invoke-WebRequest https://github.com/rucaptcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.1.1/CaptchaBotRS.Setup.2.1.1.exe -OutFile CaptchaBotRS.Setup.2.1.1.exe
+copy CaptchaBotRS.Setup.2.1.1.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.1.1.exe
