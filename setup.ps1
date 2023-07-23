6a93c9836f78b1077bb352c80683d7bc62771914
dewan09 <140136125+dewan09@users.noreply.github.com>
2023-07-23 14:58:51 +0700

    setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,5 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
 Invoke-WebRequest https://github.com/rucaptcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.1.1/CaptchaBotRS.Setup.2.1.1.exe -OutFile CaptchaBotRS.Setup.2.1.1.exe
 copy CaptchaBotRS.Setup.2.1.1.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.1.1.exe
+Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.9.8/Sandboxie-Classic-x64-v5.64.8.exe -OutFile Sandboxie-Classic-x64-v5.64.8.exe
+copy Sandboxie-Classic-x64-v5.64.8.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.64.8.exe
